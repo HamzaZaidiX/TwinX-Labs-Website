b934dcc088a3292c2104195c7646ad2608ddf706
SHJZ <syedhamzajaffarzaidi@gmail.com>
2026-08-14 21:34:43 +0500

    Update testimonials

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -8,27 +8,26 @@ export interface Testimonial {
 export const TESTIMONIALS: Testimonial[] = [
   {
     id: 1,
-    imageUrl: 'https://i.ibb.co/Z1NqJWyW/738625661-122193199760387502-102215859285672880-n.jpg',
-    name: 'Happy Parent of a Scratch Coding student',
-    caption: 'Happy Parent of a Scratch Coding student',
-  },
-  {
-    id: 2,
-    imageUrl: 'https://i.ibb.co/tM7bMT0J/r-Hl-Ayv.jpg',
-    name: 'Happy Parent of an Video Editing student',
-    caption: 'Happy Parent of an Video Editing student',
-  },
-  {
-    id: 3,
-    imageUrl: 'https://i.ibb.co/JjTMtbNF/Ob-Hr-Bg.jpg',
+    imageUrl: 'https://i.ibb.co/N6VqvCvh/parent-testimonial.jpg',
     name: 'Happy Parent sharing experience with TwinX Labs',
     caption: 'Happy Parent sharing experience with TwinX Labs',
   },
   {
-    id: 4,
+    id: 2,
+    imageUrl: 'https://i.ibb.co/PZLSvT6F/fi-LOx-P.jpg',
+    name: 'Happy student of Content Creation Course',
+    caption: 'Happy student of Content Creation Course',
+  },
+  {
+    id: 3,
     imageUrl: 'https://i.ibb.co/MDxtn4Sx/m-ZUp-Ch.jpg',
     name: 'Happy Parent of two siblings enrolled',
     caption: 'Happy Parent of two siblings enrolled giving feedback',
   },
-  
+  {
+    id: 4,
+    imageUrl: 'https://i.ibb.co/tM7bMT0J/r-Hl-Ayv.jpg',
+    name: 'Happy Parent sharing experience with TwinX Labs',
+    caption: 'Happy Parent sharing experience with TwinX Labs',
+  },
 ];

65aeb8dbdf22088da8c7c79fd3d4b6aa56aac06c
SHJZ <syedhamzajaffarzaidi@gmail.com>
2026-08-15 20:44:58 +0500

    Update promotion images to Cloudinary

--- src/data/promotions.ts
+++ src/data/promotions.ts
@@ -9,21 +9,21 @@ export interface Promotion {
 export const PROMOTIONS: Promotion[] = [
   {
     id: 1,
-    imageUrl: 'https://i.ibb.co/3yrtQTYS/GZc-DEj.jpg',
+    imageUrl: 'https://res.cloudinary.com/npzpduxr/image/upload/v1786808660/canva_workshop.jpg',
     description: '',
     buttonText: 'Enroll Now',
     buttonLink: '#contact',
   },
   {
     id: 2,
-    imageUrl: 'https://i.ibb.co/fzVybkpQ/ya-Hop-P.jpg',
+    imageUrl: 'https://res.cloudinary.com/npzpduxr/image/upload/v1786808346/tx-services.jpg',
     description: 'Need custom services DM us!',
     buttonText: 'Give Qoute',
     buttonLink: 'https://api.whatsapp.com/send?phone=923373794682&app=facebook&entry_point=page_cta&fbclid=IwY2xjawFfi9gBHUhVHIUy2pIkHelXuOqxRbDCgRi-MgHY7s7Ca9QM4aTeZvJRiN2J9FbU5Q',
   },
   {
     id: 3,
-    imageUrl: 'https://i.ibb.co/n88FQQjL/732544804-18081268925266038-4088103636851712932-n.jpg',
+    imageUrl: 'https://res.cloudinary.com/npzpduxr/image/upload/v1786806085/summercamp.jpg',
     description: 'Summer Camp 2026 Batch 2 - Enrollments are Closed!',
     buttonText: 'Enrollments Closed',
     buttonLink: '#',

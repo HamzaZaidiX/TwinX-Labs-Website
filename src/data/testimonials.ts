5a104541f4ee3d533b6303a5f90de3384eaeaa96
SHJZ <syedhamzajaffarzaidi@gmail.com>
2026-08-15 20:00:44 +0500

    Update testimonial image to Cloudinary

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -8,25 +8,25 @@ export interface Testimonial {
 export const TESTIMONIALS: Testimonial[] = [
   {
     id: 1,
-    imageUrl: 'https://i.ibb.co/N6VqvCvh/parent-testimonial.jpg',
+    imageUrl: 'https://res.cloudinary.com/npzpduxr/image/upload/v1786805886/parent-testimonial.jpg',
     name: 'Happy Parent sharing experience with TwinX Labs',
     caption: 'Happy Parent sharing experience with TwinX Labs',
   },
   {
     id: 2,
-    imageUrl: 'https://i.ibb.co/PZLSvT6F/fi-LOx-P.jpg',
+    imageUrl: 'https://res.cloudinary.com/npzpduxr/image/upload/v1786805938/fi-LOx-P.jpg',
     name: 'Happy student of Content Creation Course',
     caption: 'Happy student of Content Creation Course',
   },
   {
     id: 3,
-    imageUrl: 'https://i.ibb.co/MDxtn4Sx/m-ZUp-Ch.jpg',
+    imageUrl: 'https://res.cloudinary.com/npzpduxr/image/upload/v1786805997/m-ZUp-Ch.jpg',
     name: 'Happy Parent of two siblings enrolled',
     caption: 'Happy Parent of two siblings enrolled giving feedback',
   },
   {
     id: 4,
-    imageUrl: 'https://i.ibb.co/N6VqvCvh/parent-testimonial.jpg',
+    imageUrl: 'https://res.cloudinary.com/npzpduxr/image/upload/v1786805886/parent-testimonial.jpg',
     name: 'Happy Parent sharing experience with TwinX Labs',
     caption: 'Happy Parent sharing experience with TwinX Labs',
   },
